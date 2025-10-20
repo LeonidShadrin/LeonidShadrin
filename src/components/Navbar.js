d704c4852d70cdb86f2ec371f3208fab78f6c11f
Leonid <bolwoy97@gmail.com>
2025-10-20 14:31:45 +0300

    images changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,16 +4,15 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav className="Navbar">
-      <ul>
-        <li style={{marginRight: '100px'}}  ><Link to="/">Головна | </Link></li>
+      <ul style={{ marginLeft: '100px' }}>
         <li><Link to="/cherhovy-chastyny">Черговий частини</Link></li>
         <li><Link to="/kitchen">Їдальня</Link></li>
         <li><Link to="/roty">Роти</Link></li>
         <li><Link to="/divisions">Підрозділи</Link></li>
 
-        <li 
-        style={{marginLeft: '100px'}} 
-        onClick={() => { localStorage.removeItem('userPsw'); window.location.reload(); }}
+        <li
+          style={{ marginLeft: '80px' }}
+          onClick={() => { localStorage.removeItem('userPsw'); window.location.reload(); }}
         > | Вихід</li>
       </ul>
     </nav>
